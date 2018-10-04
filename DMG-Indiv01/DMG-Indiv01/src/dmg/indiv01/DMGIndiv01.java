db60578a1b86cc9be76349e630efed714940f271
DAVIDMGDAW <43855580+DAVIDMGDAW@users.noreply.github.com>
2018-10-05 01:40:24 +0200

    Add files via upload

---
+++ DMG-Indiv01/DMG-Indiv01/src/dmg/indiv01/DMGIndiv01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmg.indiv01;
+
+/**
+ *
+ * @author David_daw
+ */
+public class DMGIndiv01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Numero=13;
+        String Nombre="David Martin Garcia";
+        
+        System.out.println("Variable Numero = "+ Numero);
+         System.out.println("Mi nombre es "+ Nombre);
+        
+    }
+    
+}
